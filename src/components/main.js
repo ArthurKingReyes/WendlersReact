51975487703aa375246e9c166f1102a3e5c95308
Arthur Reyes <arthurreyes90@gmail.com>
2019-07-29 20:04:58 -0700

    Added week percentage values

--- src/components/main.js
+++ src/components/main.js
@@ -9,7 +9,15 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import Radio from '@material-ui/core/Radio';
 
+// Week percentages
+const week1 = [0.65, 0.75, 0.85];
+const week2 = [0.7, 0.8, 0.9];
+const week3 = [0.75, 0.85, 0.95];
+const deload = [0.4, 0.5, 0.6];
+
 class Main extends Component {
+    
+    
     state = {
         max_weight: 0
     }
@@ -38,12 +46,14 @@ class Main extends Component {
 
                     {/* Radio buttons for week selection */}
                     <RadioGroup row> 
-                        <FormControlLabel value="week1" control={<Radio />} label="Week 1" labelPlacement="bottom"/>
+                        <FormControlLabel value="week1" control={<Radio />} label="Week 1" labelPlacement="bottom" onChange={() => console.log("hello")}/>
                         <FormControlLabel value="week2" control={<Radio />} label="Week 2" labelPlacement="bottom"/>
                         <FormControlLabel value="week3" control={<Radio />} label="Week 3" labelPlacement="bottom"/>
                         <FormControlLabel value="deload" control={<Radio />} label="Deload" labelPlacement="bottom"/>
                     </RadioGroup>
                 </FormControl>
+
+
               </Paper>             
             </Container>
           </div>
